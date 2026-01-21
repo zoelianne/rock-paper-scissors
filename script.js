8f2522deaf46236c4b93008093c3e1443cdb6c6c
zoelianne <zoexlianne@gmail.com>
2026-01-21 16:31:48 -0500

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomChoice = Math.random();
+    if (randomChoice <= 0.33) {
+        return "rock";
+    } else if (randomChoice <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
